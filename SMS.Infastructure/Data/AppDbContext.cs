18e15f09e84cdace6d2f30cf2db757ab55e128ae
jnlllbsn <j.labsan.548631@umindanao.edu.ph>
2025-03-27 15:26:00 +0800

    added Database

---
+++ SMS.Infastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Domain;
+//removed unnecessary usings
+namespace SMS.Infastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=db_students;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"); //copy and paste from localdb connection string
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Student> Students { get; set; } //added references
+        public DbSet<Programs> Programs { get; set; }
+    }
+}
